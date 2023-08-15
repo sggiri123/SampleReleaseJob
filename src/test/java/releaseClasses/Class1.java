8943534412abce84217998615f1b891c44fbe64f
Shridhar Giri <Shridhar.Giri@ivycomptech.com>
2023-08-15 16:20:56 +0530

    COmmiting releasejob

---
+++ src/test/java/releaseClasses/Class1.java
@@ -0,0 +1,12 @@
+package releaseClasses;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public void release1()
+	{
+		System.out.println("Executed code for release process.");
+	}
+}
---
+++ src/test/java/releaseClasses/Class2.java
@@ -0,0 +1,11 @@
+package releaseClasses;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+	@Test
+	public void release2()
+	{
+		System.out.println("Successfully released to Prod.");
+	}
+}
